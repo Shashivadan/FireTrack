49991e6fa3f895d3a5d3287f423d6528b8b60856
Shashivadan <shashivadan99@gmail.com>
2024-04-06 00:01:29 +0530

    added: Skeleton loader

--- Frontend/src/pages/Assistanse.tsx
+++ Frontend/src/pages/Assistanse.tsx
@@ -1,13 +1,19 @@
 import Nav from "@/components/Nav";
-
+import { Input } from "@/components/ui/input";
 function Assistanse() {
   return (
     <>
-      <div className="w-full md:max-w-screen-xl m-auto">
+      <div className="w-full md:max-w-screen-xl m-auto px-3 md:px-2">
         <Nav />
         <div className="h-[calc(100vh-60px)] py-6">
           <div className=" h-full w-full bg-slate-800 rounded-2xl">
-            <div className=""></div>
+            <div className="h-[90%]  w-full "></div>
+            <div className=" h-[10%] flex justify-center items-center mx-2 md:px-2">
+              <Input
+                type="text"
+                className=" h-16 border-slate-500 border-none bg-slate-700 rounded-2xl text-xl leading-normal"
+              />
+            </div>
           </div>
         </div>
       </div>
