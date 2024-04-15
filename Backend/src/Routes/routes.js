7b8fd68e05cd94995dc7e6dc4f317a72d83efdfe
Shashivadan <shashivadan99@gmail.com>
2024-04-15 21:58:36 +0530

    Added: end points to records

--- Backend/src/Routes/routes.js
+++ Backend/src/Routes/routes.js
@@ -7,6 +7,7 @@ import { authMiddleWare } from "../middleware/authMiddeWare.js";
 
 import { model_api } from "../controllers/api.model/api.model.js";
 import geminiApi from "../controllers/gemini.api/gemini.api.js";
+import displayRecords from "../controllers/Records/records.js";
 
 const router = Router();
 
@@ -17,5 +18,8 @@ router.post("/signup", signUp);
 // model route
 router.get("/model", authMiddleWare, model_api);
 router.post("/gemini", geminiApi);
+
+// display user records
+router.post("/records", authMiddleWare, displayRecords);
 
 export { router };
---
+++ Backend/src/controllers/Records/records.js
@@ -0,0 +1,27 @@
+import Records from "../../models/Records.js";
+
+async function displayRecords(req, res) {
+  try {
+    const userId = req.userId;
+    console.log(userId);
+    const records = await Records.find({ userId: userId });
+    return res.status(200).json({
+      records: records.map((record) => {
+        return {
+          _id: record._id,
+          temperature: record.temperature,
+          oxygen: record.oxygen,
+          humidity: record.humidity,
+          danger: record.danger,
+          probability: record.probability,
+          createdAt: record.createdAt,
+        };
+      }),
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+}
+
+export default displayRecords;
